20fe06c12011a79ab52389552f81d3c46bbd59ca
Sarafina <sarafinajozefzoon@hotmail.com>
2018-04-06 01:11:30 +0200

    media-queries done

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // fetch('https://api.artsy.net/api/genes/cuba')
 
-var map;
+var map, infoWindow;
 
 //locatie van het vliegveld wordt hier geselecteerd
 var airportmap = {
@@ -69,13 +69,6 @@ function initMap() {
               'styled_map']
     }
   }); 
-
-   // map = new google.maps.Map(document.getElementById('map-canvas'),
-   //      mapOptions);
-   //  // Add interaction listeners to make weather requests
-   //  google.maps.event.addListener(map, 'idle', checkIfDataRequested);
-
-    
 
   // var image = {
   //   url: 'images/marker.svg',
@@ -176,6 +169,8 @@ function initMap() {
     });
   }
 
+  //De markers om de locaties
+
   //De opmaak van de kaart
   var styledMapType = new google.maps.StyledMapType(
       [
@@ -233,154 +228,9 @@ function initMap() {
   //Laten weergeven van de gestylde map
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
-
-  // //icon marker voor de locaties
-  // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 }
 
-  var map;
-  var geoJSON;
-  var request;
-  var gettingData = false;
-  var openWeatherMapKey = "AIzaSyDpVHur421EgO8CCZiHJ5cD_Yh17YuA3TM"
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 4,
-      center: new google.maps.LatLng(22.15,-80.41388699999999)
-    };
-
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-    // Add interaction listeners to make weather requests
-    google.maps.event.addListener(map, 'idle', checkIfDataRequested);
-
-    // Sets up and populates the info window with details
-    map.data.addListener('click', function(event) {
-      infowindow.setContent(
-       "<img src=" + event.feature.getProperty("icon") + ">"
-       + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
-       + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
-       + "<br />" + event.feature.getProperty("weather")
-       );
-      infowindow.setOptions({
-          position:{
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng()
-          },
-          pixelOffset: {
-            width: 0,
-            height: -15
-          }
-        });
-      infowindow.open(map);
-    });
-  }
-
-  var checkIfDataRequested = function() {
-    // Stop extra requests being sent
-    while (gettingData === true) {
-      request.abort();
-      gettingData = false;
-    }
-    getCoords();
-  };
-
-  // Get the coordinates from the Map bounds
-  var getCoords = function() {
-    var bounds = map.getBounds();
-    var NE = bounds.getNorthEast();
-    var SW = bounds.getSouthWest();
-    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
-  };
-
-  // Make the weather request
-  var getWeather = function(northLat, eastLng, southLat, westLng) {
-    gettingData = true;
-    var requestString = "https://api.openweathermap.org/data/2.5/box/city?bbox="
-                        + westLng + "," + northLat + "," //left top
-                        + eastLng + "," + southLat + "," //right bottom
-                        + map.getZoom()
-                        + "&cluster=yes&format=json"
-                        + "&APPID=" + openWeatherMapKey;
-    request = new XMLHttpRequest();
-    request.onload = proccessResults;
-    request.open("get", requestString, true);
-    request.send();
-  };
-
-  // Take the JSON results and proccess them
-  var proccessResults = function() {
-    console.log(this);
-    var results = JSON.parse(this.responseText);
-    if (results.list.length > 0) {
-        resetData();
-        for (var i = 0; i < results.list.length; i++) {
-          geoJSON.features.push(jsonToGeoJson(results.list[i]));
-        }
-        drawIcons(geoJSON);
-    }
-  };
-
-  var infowindow = new google.maps.InfoWindow();
-
-  // For each result that comes back, convert the data to geoJSON
-  var jsonToGeoJson = function (weatherItem) {
-    var feature = {
-      type: "Feature",
-      properties: {
-        city: weatherItem.name,
-        weather: weatherItem.weather[0].main,
-        temperature: weatherItem.main.temp,
-        min: weatherItem.main.temp_min,
-        max: weatherItem.main.temp_max,
-        humidity: weatherItem.main.humidity,
-        pressure: weatherItem.main.pressure,
-        windSpeed: weatherItem.wind.speed,
-        windDegrees: weatherItem.wind.deg,
-        windGust: weatherItem.wind.gust,
-        icon: "https://openweathermap.org/img/w/"
-              + weatherItem.weather[0].icon  + ".png",
-        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
-      }
-    };
-    // Set the custom marker icon
-    map.data.setStyle(function(feature) {
-      return {
-        icon: {
-          url: feature.getProperty('icon'),
-          anchor: new google.maps.Point(25, 25)
-        }
-      };
-    });
-
-    // returns object
-    return feature;
-  };
-
-  // Add the markers to the map
-  var drawIcons = function (weather) {
-     map.data.addGeoJson(geoJSON);
-     // Set the flag to finished
-     gettingData = false;
-  };
-
-  // Clear data layer and geoJSON
-  var resetData = function () {
-    geoJSON = {
-      type: "FeatureCollection",
-      features: []
-    };
-    map.data.forEach(function(feature) {
-      map.data.remove(feature);
-    });
-  };
-
-  google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
